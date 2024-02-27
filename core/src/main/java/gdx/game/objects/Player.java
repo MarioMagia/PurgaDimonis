6fbe06b96cdcebac5aa66c01dcd775b582eea735
Mario Ramirez <a22marramgar@inspedralbes.cat>
2024-02-27 22:00:47 +0100

    world physics

--- core/src/main/java/gdx/game/objects/Player.java
+++ core/src/main/java/gdx/game/objects/Player.java
@@ -8,6 +8,11 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 
@@ -29,6 +34,8 @@ public class Player extends Actor {
 
     private int direction;
 
+    private BodyDef bodyDef;
+    private FixtureDef fixtureDef;
     public Player(float x, float y, float scale) {
         this.width = Math.round(Settings.PLAYER_WIDTH*scale);
         this.height = Math.round(Settings.PLAYER_HEIGHT*scale);
@@ -39,11 +46,23 @@ public class Player extends Actor {
         setBounds(position.x, position.y, width, height);
         setTouchable(Touchable.enabled);
         stateTime = 0f;
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(width/2,height/2);
+        bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(position.x+(width/2),position.y+(height/2));
+        fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 90;
+        fixtureDef.friction = 0;
+        fixtureDef.restitution = 0;
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
+
         // Movem l'spacecraft depenent de la direcció controlant que no surti de la pantalla
         switch (direction) {
             case PLAYER_LEFT:
@@ -94,6 +113,30 @@ public class Player extends Actor {
     // Posem l'Spacecraft al seu estat original
     public void goStraight() {
         direction = PLAYER_STRAIGHT;
+    }
+
+    public BodyDef getBodyDef() {
+        return bodyDef;
+    }
+
+    public void setBodyDef(BodyDef bodyDef) {
+        this.bodyDef = bodyDef;
+    }
+
+    public FixtureDef getFixtureDef() {
+        return fixtureDef;
+    }
+
+    public void setFixtureDef(FixtureDef fixtureDef) {
+        this.fixtureDef = fixtureDef;
+    }
+
+    public void setPosition(Vector2 position,boolean center) {
+        this.position = position;
+        if(center){
+            this.position.x = position.x - (width/2);
+            this.position.y = position.y - (height/2);
+        }
     }
 }
 
--- core/src/main/java/gdx/game/screens/GameScreen.java
+++ core/src/main/java/gdx/game/screens/GameScreen.java
@@ -1,28 +1,42 @@
 package gdx.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-import gdx.game.objects.Background;
 import gdx.game.objects.Player;
 import gdx.game.objects.ScrollHandler;
 import gdx.game.utils.Settings;
 
 public class GameScreen implements Screen {
+    private OrthographicCamera camera;
     private Stage stage;
     private Player player;
+    private Body playerbody;
     private ScrollHandler sh;
-
+    private World world;
+    private Box2DDebugRenderer debugRenderer;
     private ShapeRenderer shapeRenderer;
     private Batch batch;
     public GameScreen(){
-        OrthographicCamera camera = new OrthographicCamera(Settings.GAME_WIDTH, Settings.GAME_HEIGHT);
+        Box2D.init();
+        camera = new OrthographicCamera(Settings.GAME_WIDTH, Settings.GAME_HEIGHT);
         camera.setToOrtho(false, Settings.GAME_WIDTH,Settings.GAME_HEIGHT);
         FitViewport fitViewport = new FitViewport(Settings.GAME_WIDTH,Settings.GAME_HEIGHT);
         shapeRenderer = new ShapeRenderer();
@@ -30,10 +44,38 @@ public class GameScreen implements Screen {
         batch = stage.getBatch();
         sh = new ScrollHandler();
         stage.addActor(sh);
-        player = new Player( Settings.PLAYER_STARTX,Settings.PLAYER_STARTY,4);
+        player = new Player( Settings.PLAYER_STARTX,Settings.PLAYER_STARTY+200,4);
         stage.addActor(player);
 
+        createWorld();
     }
+
+    private void createWorld() {
+        world = new World(new Vector2(0, -100), true);
+        debugRenderer = new Box2DDebugRenderer();
+
+        //Ground
+        BodyDef groundBodyDef = new BodyDef();
+        // Set its world position
+        groundBodyDef.position.set(new Vector2(0, 10));
+
+        // Create a body from the definition and add it to the world
+        Body groundBody = world.createBody(groundBodyDef);
+
+        // Create a polygon shape
+        PolygonShape groundBox = new PolygonShape();
+        // Set the polygon shape as a box which is twice the size of our view port and 20 high
+        // (setAsBox takes half-width and half-height as arguments)
+        groundBox.setAsBox(camera.viewportWidth, 90.0f);
+        // Create a fixture from our polygon shape and add it to our ground body
+        groundBody.createFixture(groundBox, 0.0f);
+
+        // Create our body in the world using our body definition
+        playerbody = world.createBody(player.getBodyDef());
+        // Create our fixture and attach it to the body
+        Fixture fixture = playerbody.createFixture(player.getFixtureDef());
+    }
+
     @Override
     public void show() {
 
@@ -44,10 +86,16 @@ public class GameScreen implements Screen {
         ScreenUtils.clear(0, 0, 0, 1);
         stage.draw();
         stage.act(delta);
-        drawElements();
+        world.step(1/60f, 6, 2);
+        Gdx.app.log("position",playerbody.getPosition()+"");
+        player.setPosition(playerbody.getPosition(),true);
+
+
+        //drawElements();
     }
 
     private void drawElements(){
+        debugRenderer.render(world, camera.combined);
         shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(Color.RED);
